1a55972de843180d6080b912f506309d564335a1
zhangsheng <1070683532@qq.com>
2020-10-12 18:06:09 +0800

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { LgetItem, LreItem, LsetItem } from '../utils/storage'
+import cfg from '@/config'
+
+if(cfg.version !== LgetItem('version') || LgetItem('version') === null){
+  localStorage.clear()
+  LsetItem('version',cfg.version)
+}
+LreItem('ossHost')
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    ossHost:LgetItem('ossHost')
   },
   mutations: {
+    SET_OSSHOST(state,data){
+      LsetItem('ossHost',data)
+      state.ossHost=data
+    }
+  },
+  getters:{
+    ossHost:state=>state.ossHost
   },
   actions: {
   },
