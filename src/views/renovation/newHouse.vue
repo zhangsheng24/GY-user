1dd969cdf0055f0c227775c883f263aff9689f68
zhangsheng <1070683532@qq.com>
2020-10-12 18:03:42 +0800

    装修流程

---
+++ src/views/renovation/newHouse.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="newHouse">
+        <div class="main">
+            <img src="@/assets/images/newHouse.png" alt="">
+            <div class="section stepBox">
+                <p>装修流程</p>
+                <step/>
+            </div>
+            <div class="section section2">
+                <img src="@/assets/images/newHouse2.png" alt="">
+            </div>
+            <div class="section section3">
+                <img src="@/assets/images/newHouse3.png" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import step from './template/step'
+export default {
+    components:{
+        step
+    }
+}
+</script>>
+<style lang="scss" scoped>
+.newHouse{
+    min-height: 100vh;
+    width: 100%;
+    .main{
+        position: relative;
+        font-size: 0;
+        >img{
+            width: 100%;
+        }
+        .section{
+            >img{
+                width: 100%;
+            }
+            &.stepBox{
+            position: absolute;
+            top: 283px;
+            left: 10px;
+            height: 132px;
+            background: #fff;
+        }
+        }
+        
+    }
+}
+</style>
---
+++ src/views/renovation/template/step.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="step">
+        <div class="stepBox" v-for="(item,index) in steps" :key="index">
+            <div class="stepInfo">{{item.title}}</div>
+            <van-icon class="arrow" name="arrow"/>
+        </div>
+    </div>
+</template>
+<script>
+import api from '@/api'
+export default {
+    data(){
+        return{
+            steps:[]
+        }
+    },
+    created(){
+      api('renovation.getPhaseDataByTypeDictId',{typeDictId:this.$route.query.typeDictId}).then(res=>{
+          this.steps=res
+      }) 
+    }
+}
+</script>>
+<style lang="scss" scoped>
+@import '@/commons/css/mixin.scss';
+.step{
+    display: flex;
+    .stepBox{
+        display: flex;
+        flex: 1;
+        align-items: center;
+        margin-right: 16px;
+        .stepInfo{
+            height: 40px;
+            line-height: 40px;
+            width: 100%;
+            text-align: center;
+            font-size: 12px;
+            position: relative;
+            @include border-1px(#20b7b6,'all',4px,solid);
+        }
+        .arrow{
+            font-size: 10px;
+            color: #20B7B6;
+        }
+
+    }
+}
+</style>
